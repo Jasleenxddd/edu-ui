852ccee95dfbe048af82ca9828b0928fe312df99
Jasleen Kaur <jasleenkaurvasir@gmail.com>
2025-05-22 20:50:20 +0530

    first commit

--- src/components/account_settings.jsx
+++ src/components/account_settings.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-
+import image from '../images.jpeg'
 const AccountSettings = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="flex flex-col items-center min-h-screen bg-white px-4">
+    <div className="flex flex-col items-center min-h-screen bg-white px-4 py-10 sm:py-20">
       {/* Card Container */}
-      <div className="w-full max-w-sm bg-white border border-gray-200 rounded-xl p-6 shadow-md mt-48">
-        <h2 className="text-xl font-semibold text-gray-900 mb-4">
+      <div className="w-full max-w-sm bg-white border border-gray-200 rounded-xl p-6 shadow-md">
+        <h2 className="text-xl font-semibold text-gray-900 mb-4 text-center">
           Account Settings
         </h2>
 
         {/* Profile Info */}
-        <div className="flex items-start gap-4 mb-6">
+        <div className="flex flex-col sm:flex-row items-center sm:items-start gap-4 mb-6">
           {/* Avatar with edit icon */}
-          <div className="relative w-16 h-16">
-            <img
-              src="https://via.placeholder.com/64"
-              alt="Avatar"
-              className="w-16 h-16 rounded-full object-cover"
-            />
+          <div className="w-16 h-16 rounded-full overflow-hidden flex items-center justify-center bg-gray-100">
+  <img
+    src={image}
+    alt="Avatar"
+    className="w-full h-full object-cover"
+  />
             <div className="absolute bottom-0 right-0 w-5 h-5 bg-[#6C25FF] rounded-full flex items-center justify-center">
               <svg
                 className="text-white w-3 h-3"
@@ -33,7 +33,7 @@ const AccountSettings = () => {
           </div>
 
           {/* User Info */}
-          <div className="flex-1">
+          <div className="text-center sm:text-left">
             <p className="font-semibold text-gray-900">Marry Doe</p>
             <p className="text-sm text-gray-500 mb-2">Marry@Gmail.Com</p>
             <p className="text-sm text-gray-700">
